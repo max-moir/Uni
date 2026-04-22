36b2a33f1282fee8ddea562c5fab2a809223705a
maxwmoir@gmail.com <maxwmoir@gmail.com>
2026-04-22 18:01:27 +1200

    Set up system, invariants not finished.

--- C470/Learn/Learn/Basic.lean
+++ C470/Learn/Learn/Basic.lean
@@ -5,50 +5,63 @@ set_option linter.dupNamespace false
 veil module MyMutex
 
 type node
+type ticket
 
 instantiate tot : TotalOrder node
 
+enum state = {idle, waiting, critical}
 -- Nodes
 relation critical : node → Prop
 function choosing : node → Prop
-function number   : node → Prop
-enum state = {idle, waiting, critical}
-
+function number   : node → ℕ
 function cs : node → state
-function t : node → ℕ
-function serving : node → ℕ
-
-
 
 #gen_state
-
 
 #print State
 
 after_init {
   critical N := False;
   choosing N := False;
-  number N := False;
-  serving N := 0;
-  cs N := idle;
-  t N := 0;
-
+  number N := 0;
+  cs N  := idle;
 }
 
 #print initialState?
 
+
 action choose (n : node)  = {
-  require ¬ choosing n ;
+  require ¬ choosing n;
   choosing n := True;
-  number n := True;
+
+  -- Find ticket value greater than all others
+  let t_max ← fresh;
+  require ∀ j, j ≠ n → number j < t_max;
+  number n := t_max;
+
+  choosing n := False;
+}
+
+action enter (n : node) = {
+  require number n != 0;
+
+  require ∀ j, j ≠ n →
+    (¬ choosing j) ∧ ((number j = 0) ∨ (number n < number j) ∨ (number n = number j ∧ tot.le n j));
+
+  critical n := True;
 }
 
 action exit (n : node) = {
-  require critical n;
-  critical n := False;
-
+  require critical n
+  number n := 0
+  critical n := False
 }
 
 
+safety [mutex] critical I ∧ critical J → I = J
+
+
+#gen_spec
+#check_invariants
 
 end MyMutex
