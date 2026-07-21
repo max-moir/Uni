a549df9abb180c95d85796e95fc39e11a92c2350
mmo199@uclive.ac.nz <mmo199@uclive.ac.nz>
2026-07-21 15:26:29 +1200

    started dl assignment

---
+++ deep-learning/assignments/1/COSC440_Assignment1/preprocess.py
@@ -0,0 +1,55 @@
+import gzip
+import shutil
+import numpy as np
+import numpy.typing as npt
+
+INPUT_HEADER_SIZE = 16
+LABEL_HEADER_SIZE = 8
+
+def get_data(inputs_file_path : str, labels_file_path: str, num_examples: int):
+    """
+    Takes in an inputs file path and labels file path, unzips both files,
+    normalizes the inputs, and returns (NumPy array of inputs, NumPy
+    array of labels). Read the data of the file into a buffer and use
+    np.frombuffer to turn the data into a NumPy array. Keep in mind that
+    each file has a header of a certain size. This method should be called
+    within the main function of the assignment.py file to get BOTH the train and
+    test data. If you change this method and/or write up separate methods for
+    both train and test data, we will deduct points.
+
+    Hint: look at the writeup for sample code on using the gzip library
+
+    :param inputs_file_path: file path for inputs, something like
+    'MNIST_data/t10k-images-idx3-ubyte.gz'
+    :param labels_file_path: file path for labels, something like
+    'MNIST_data/t10k-labels-idx1-ubyte.gz'
+    :param num_examples: used to read from the bytestream into a buffer. Rather
+    than hardcoding a number to read from the bytestream, keep in mind that each image
+    (example) is 28 * 28, with a header of a certain number.
+    :return: NumPy array of inputs as float32 and labels as int8
+    """
+
+    # TODO: Load inputs and labels
+    inputs = [] 
+    labels = [] 
+
+    with gzip.open(inputs_file_path, 'rb') as f:
+        f.read(INPUT_HEADER_SIZE)
+
+        for i in range(num_examples):
+            buffer = f.read(784)
+            A = np.frombuffer(buffer, np.uint8)
+            Normalized = (A.astype(np.uint32) / 255).astype(np.float32)
+            inputs.append(Normalized)
+        
+    with gzip.open(labels_file_path, 'rb') as f:
+        f.read(LABEL_HEADER_SIZE)
+
+        for _ in range(num_examples):
+            buffer = f.read(1)
+            A = np.frombuffer(buffer, np.uint8)
+            labels.append(A)
+
+    return inputs, labels
+
+
